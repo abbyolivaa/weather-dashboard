55cdda63e0228092a7c2aab8b1b8c19fa34cd728
Abby Oliva <abbyoliva6@icloud.com>
2022-04-09 13:17:20 -0600

    searchbar history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,40 +1,36 @@
-var searchBtn = document.querySelector(".search-container button")
-var h2 = document.querySelector(".current-container h2")
-var cityInput = document.querySelector("input[name='city-name']").value;
-console.log(cityInput)
+var searchBtn = $(".search-container button")
+var h2 = $(".current-container h2")
 
 // city input value
 var date = moment().format("M/D/YYYY")
 
 var apiKey =  "&appid=a27be585bc997b59e02877d017f61c9d";
 
-//search button click
-searchBtn.addEventListener("click", function(){
-    h2.textContent = cityInput + date
+//key count for local storage
+
+searchBtn.click(function () {
+    let searchInput = $(".search-bar").val();
+    // Variable for current weather working 
+    let urlCurrent = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInput + apiKey + "&units=imperial";
+    // Variable for 5 day forecast working
+    let urlFiveDay = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchInput + apiKey + "&units=imperial";
+
+    if(searchInput===""){
+        console.log(searchInput);
+    } else {
+        $.ajax({
+            url: urlCurrent,
+            method: "GET"
+        }).then(function(response){
+            // list-group append an li to it with just set text
+            console.log(response.name);
+            let cityName = $(".list-group").addClass("list-group-item");
+            cityName.append("<li>" + response.name + "</li>");
+            // Local storage
+            let local = localStorage.setItem(keyCount, response.name);
+            keyCount = keyCount++;
+        })
+    
+    }   
+
 })
-
-
-var cityInputHandler = function(event){
-    event.preventDefault();
-    
-    //check if input is valid
-    if(!cityInput){
-        alert("You need to search for a city!");
-        return false;
-    }
-
-    //reset input form
-    document.querySelector("input[name='city-name']").value = "";
-    
-
-
-}
-
-
-
-
-
-
-
-
-    
